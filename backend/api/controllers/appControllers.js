1b21a61c317c13e02586d45eb8e8610be7b0bcb5
ASTRohit <aaryavart.rohit.chaskar@gmail.com>
2018-09-08 15:24:01 +0530

    duplicate variable name

--- backend/api/controllers/appControllers.js
+++ backend/api/controllers/appControllers.js
@@ -1,15 +1,15 @@
-var user = require('./userController');
-var location = require('./locationController');
-var stream = require('./streamController');
-var mapping = require('./mappingController');
+var userCtrl = require('./userController');
+var locationCtrl = require('./locationController');
+var streamCtrl = require('./streamController');
+var mappingCtrl = require('./mappingController');
 
 function register(req, res, next) {
 	var user = req.body;
 	console.log('Request: '+JSON.stringify(user));
-	user.parent_id = parseInt(user.parent_id);
-	user.status = parseInt(user.status);
-	user.is_admin = getBoolean(user.is_admin);
-	user.insertUser(user)
+	// user.parent_id = parseInt(user.parent_id);
+	// user.status = parseInt(user.status);
+	// user.is_admin = getBoolean(user.is_admin);
+	userCtrl.insertUser(user)
 		.then(function () {
 			res
 				.status(200)
