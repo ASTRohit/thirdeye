ef003f75c2df6c7995650a4e2cd4bcbf5b27d483
ASTRohit <aaryavart.rohit.chaskar@gmail.com>
2018-09-08 15:11:50 +0530

    table name updated

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -2,7 +2,7 @@ var connection = require('../utility/connection');
 var db = connection.database;
 
 function insert(user){
-	var insertQuery='INSERT INTO user_master(name, email, mobile, parent_id, is_admin, salt, password, status)	'+
+	var insertQuery='INSERT INTO third_eye.user_master(name, email, mobile, parent_id, is_admin, salt, password, status) '+
 	'VALUES (${name}, ${email}, ${mobile}, ${parent_id}, ${is_admin}, ${salt}, ${password}, ${status});';
 	return db.none(insertQuery,user);	
 }
